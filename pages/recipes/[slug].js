3622d67595391ecc32c4d7b266869d1587e191dc
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-07-24 03:19:11 -0700

    again

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful'
 import Image from 'next/image'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Skeleton from '../../components/Skeleton'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -38,7 +39,7 @@ export const getStaticProps = async ({ params }) => {
 }
 
 export default function RecipeDetails({ recipe }) {
-  if(!recipe) return <div>loading</div>
+  if(!recipe) return <Skeleton />
   
   const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
   
